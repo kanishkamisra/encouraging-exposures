6591ffd0adbaae2badd8b0f95d8901863f93ae82
Kanishka Misra <menogetusername@gmail.com>
2024-04-03 00:33:07 -0500

    givenness stimuli

--- src/givenness.py
+++ src/givenness.py
@@ -29,28 +29,37 @@ generalization = utils.read_jsonl("data/experiments/single_stimuli_dative_simula
 
 for t_id, template in TEMPLATES.items():
     save_dir_tg = f"data/experiments/single_stimuli_dative_simulation_valtest_vbd_discourse_theme_given_template_{t_id}"
-    save_dir_rg = f"data/experiments/single_stimuli_dative_simulation_valtest_vbd_discourse_recipient_given_template_{t_id}"
     pathlib.Path(save_dir_tg).mkdir(parents=True, exist_ok=True)
+    
+    save_dir_rg = f"data/experiments/single_stimuli_dative_simulation_valtest_vbd_discourse_recipient_given_template_{t_id}"
     pathlib.Path(save_dir_rg).mkdir(parents=True, exist_ok=True)
+    
     #write generalization as is:
     write_jsonl(generalization, f"{save_dir_tg}/generalization.jsonl")
     write_jsonl(generalization, f"{save_dir_rg}/generalization.jsonl")
 
-    theme_given = adaptation.copy()
-    recipient_given = adaptation.copy()
-
-    for data in theme_given:
+    adaptation_copy_theme = adaptation.copy()
+    adaptation_copy_recipient = adaptation.copy()
+    
+    
+    theme_given = []
+    for data in adaptation_copy_theme:
         if data["theme_definiteness"] == "definite":
-            data["sentence"] = generate_givenness(data["agent"], data["theme"], data["sentence"], template)
+            sentence = data["sentence"].split(".")[-2].strip() + " ."
+            data["sentence"] = generate_givenness(data["sampled_agent"], data["theme"], sentence, template)
             data["template_id"] = t_id
             data["given"] = "theme"
+            theme_given.append(data)
 
-    for data in recipient_given:
+    write_jsonl(theme_given, f"{save_dir_tg}/adaptation.jsonl")
+
+    recipient_given = []
+    for data in adaptation_copy_recipient:
         if data["recipient_definiteness"] == "definite":
-            data["sentence"] = generate_givenness(data["agent"], data["recipient"], data["sentence"], template)
+            sentence = data["sentence"].split(".")[-2].strip() + " ."
+            data["sentence"] = generate_givenness(data["sampled_agent"], data["recipient"], sentence, template)
             data["template_id"] = t_id
             data["given"] = "recipient"
+            recipient_given.append(data)
 
-    # write both to jsonls
-    write_jsonl(theme_given, f"{save_dir_tg}/adaptation.jsonl")
     write_jsonl(recipient_given, f"{save_dir_rg}/adaptation.jsonl")
