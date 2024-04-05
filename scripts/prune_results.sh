270d581b6df6e195049b6063cb9ea301252af297
Kanishka Misra <menogetusername@gmail.com>
2024-04-04 22:46:35 -0400

    td fix for other seeds

--- scripts/prune_results.sh
+++ scripts/prune_results.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # declare -a seeds=(111 222 333 444 555 666 777 888 999 1709)
-# declare -a seeds=(6 28 221 394 496 1024 1102 1729 2309 8128)
+declare -a seeds=(6 28 221 394 496 1024 1102 1729 2309 8128)
 # declare -a seeds=(333 444 555 666)
-declare -a seeds=(6 28 221 1024 1102 1729)
+# declare -a seeds=(394 496)
 
 for seed in ${seeds[@]}
 do 
