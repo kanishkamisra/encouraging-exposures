c83f6f825933bad9bf01a6941ca63955fbc078cf
Kanishka Misra <menogetusername@gmail.com>
2024-05-18 13:38:32 -0500

    balanced genset results part2

--- scripts/balanced_gen_results.sh
+++ scripts/balanced_gen_results.sh
@@ -8,10 +8,12 @@
 
 # python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_no_discourse3 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
 
-python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_discourse_theme_given_template_1 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
+# python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_discourse_theme_given_template_1 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
 
-python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_discourse_recipient_given_template_1 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
+# python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_discourse_recipient_given_template_1 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
 
-python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_discourse_theme_given_template_3 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
+# python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_discourse_theme_given_template_3 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
 
-python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_discourse_recipient_given_template_3 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
+# python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_discourse_recipient_given_template_3 --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
+
+python src/balanced_genset_results.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_neutral_discourse --gen_path data/balanced_generalization_set_four_features.csv --device cuda:0
