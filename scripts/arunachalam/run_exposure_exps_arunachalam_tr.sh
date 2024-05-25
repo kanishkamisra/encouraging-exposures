f4308afef5d8235cc58a85bd168f80e155ba1a0a
Kanishka Misra <menogetusername@gmail.com>
2024-05-24 21:30:05 -0500

    arunachalam replications pruned results

--- scripts/arunachalam/run_exposure_exps_arunachalam_tr.sh
+++ scripts/arunachalam/run_exposure_exps_arunachalam_tr.sh
@@ -6,14 +6,21 @@ declare -a seeds=(6 28 221 394 496 1024 1102 1729 2309 8128)
 declare -a templates=(1 2 3)
 # declare -a seeds=(1024 1102 1729 2309 8128)
 
+# for template in ${templates[@]}
+# do
+#     for seed in ${seeds[@]}
+#     do
+#         for lr in ${lrs[@]}
+#         do
+#             python src/simulation.py --model_name kanishka/smolm-autoreg-bpe-seed_$seed --gaussian --lr $lr --experiment_name single_stimuli_dative_simulation_valtest_vbd_arunachalam_template_tr_$template
+#         done
+#     done
+# done
+
 for template in ${templates[@]}
 do
     for seed in ${seeds[@]}
     do
-        for lr in ${lrs[@]}
-        do
-            python src/simulation.py --model_name kanishka/smolm-autoreg-bpe-seed_$seed --gaussian --lr $lr --experiment_name single_stimuli_dative_simulation_valtest_vbd_arunachalam_template_tr_$template
-        done
+        python src/prune_results_new.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_arunachalam_template_rt_$template/smolm-autoreg-bpe-seed_$seed/raw --experiment_name single_stimuli_dative_simulation_valtest_vbd_arunachalam_template_rt_$template
     done
-    python src/prune_results_new.py --results_dir data/results/single_stimuli_dative_simulation_valtest_vbd_arunachalam_template_tr_$template/smolm-autoreg-bpe-seed_$seed/raw --experiment_name single_stimuli_dative_simulation_valtest_vbd_arunachalam_template_tr_$template
 done
